8e6d13eb9ceb0775e7d480e541c5f2c93cd94b99
ianhattwick <ian@ianhattwick.com>
2024-02-18 01:39:39 -0500

    fixed midi keyboard toggle issue

--- src/MidiKeyboard.js
+++ src/MidiKeyboard.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import keyboard from './Icons/keyboard.png';
 const midi = require('./Midi.js');
 function MidiKeyboard() {
     const [midiOn, setMidiOn] = useState(false);
     const [notesOn, setNotesOn] = useState(new Set());
+    const midiOnRef = useRef(midiOn); // Mutable reference to hold the latest value of midiOn
+
     let activeKeys = {};
     var octave = 4;
     var keyToNote = {
@@ -25,65 +27,77 @@ function MidiKeyboard() {
         186: { "midi": 75, "pitch": "D#/Eb" }, // ; (or : depending on keyboard)
         191: { "midi": 76, "pitch": "E" }      // / (or ? depending on keyboard)
     };
+
     useEffect(() => {
-        if (midiOn) {
-            document.addEventListener('keydown', handleKeyDown);
-            document.addEventListener('keyup', handleKeyUp);
-        } else {
-            document.removeEventListener('keydown', handleKeyDown);
-            document.removeEventListener('keyup', handleKeyUp);
-        }
-        // Cleanup: Remove the event listener when the component unmounts
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+
+        // Cleanup: Remove the event listeners when the component unmounts
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
             document.removeEventListener('keyup', handleKeyUp);
         };
+    },[]);
+
+    // Update the mutable reference whenever midiOn changes
+    useEffect(() => {
+        midiOnRef.current = midiOn;
     }, [midiOn]);
+    
+
     function handleKeyDown(event) {
-        const keyCode = event.keyCode;
-        if (!activeKeys[keyCode]) {
-            activeKeys[keyCode] = true;
-            try {
-                let note = keyToNote[keyCode];
-                let midiNote = note["midi"] + (octave - 4) * 12;
-                if (midiNote <= 127) {
-                    setNotesOn(new Set(notesOn).add(midiNote));
-                    midi.midiHandlerInstance.handleNoteOn(midiNote, 100);
-                }
-            } catch (error) {
-                if (keyCode === 37) {
-                    decreaseOctave();
-                } else if (keyCode === 39) {
-                    increaseOctave();
+        const midiOn = midiOnRef.current;
+        if( midiOn ){
+            const keyCode = event.keyCode;
+            if (!activeKeys[keyCode]) {
+                activeKeys[keyCode] = true;
+                try {
+                    let note = keyToNote[keyCode];
+                    let midiNote = note["midi"] + (octave - 4) * 12;
+                    if (midiNote <= 127) {
+                        setNotesOn(new Set(notesOn).add(midiNote));
+                        midi.midiHandlerInstance.handleNoteOn(midiNote, 100);
+                    }
+                } catch (error) {
+                    if (keyCode === 37) {
+                        decreaseOctave();
+                    } else if (keyCode === 39) {
+                        increaseOctave();
+                    }
+                    //console.log(octave)
                 }
             }
         }
     }
     function handleKeyUp(event) {
-        const keyCode = event.keyCode;
-        activeKeys[keyCode] = false;
-        try {
-            let note = keyToNote[keyCode];
-            let midiNote = note["midi"] + (octave - 4) * 12;
-            if (midiNote <= 127) {
-                setNotesOn(new Set(notesOn).delete(midiNote));
-                midi.midiHandlerInstance.handleNoteOff(midiNote, 0);
+        const midiOn = midiOnRef.current;
+        if( midiOn ){
+            const keyCode = event.keyCode;
+            activeKeys[keyCode] = false;
+            try {
+                let note = keyToNote[keyCode];
+                let midiNote = note["midi"] + (octave - 4) * 12;
+                if (midiNote <= 127) {
+                    setNotesOn(new Set(notesOn).delete(midiNote));
+                    midi.midiHandlerInstance.handleNoteOff(midiNote, 0);
+                }
+            } catch (error) {
             }
-        } catch (error) {
         }
     }
+
     function increaseOctave() {
-        if (octave < 10) {
+        if (octave < 7) {
             octave++;
         }
     }
     function decreaseOctave() {
-        if (octave > -2) {
+        if (octave > 1) {
             octave--;
         }
     }
     const midiClicked = () => {
-        setMidiOn(!midiOn);
+        setMidiOn(midiOn === false ? true : false);          
     }
     const keyboardCSS = midiOn ? 'icon active' : 'icon inactive';
     return (

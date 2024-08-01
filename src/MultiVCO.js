f34314d60a965dc369ba81d7e9d062d1eebd6c10
Tyler Ptak <tylerptak@gmail.com>
2024-08-01 17:22:15 -0500

    Updated MultiVCO

--- src/MultiVCO.js
+++ src/MultiVCO.js
@@ -9,10 +9,10 @@ import p5 from 'p5';
 import * as Tone from 'tone';
 
 export class MultiVCO{
-    constructor(vcos = ['triangle','sawtooth','square'], pitchshift = [1,1,1]){
+    constructor(vcos = [], pitchshift = []){
         this.numInputs = vcos.length
         this.frequency = new Tone.Signal(1)
-        this.output = new Tone.Multiply(1/this.numInputs)
+        this.output = this.numInputs === 0 ? new Tone.Multiply(1) : new Tone.Multiply(1/this.numInputs)
 
         this.freqScalars= []
         this.gainStages = []
@@ -35,6 +35,49 @@ export class MultiVCO{
             this.gainStages[this.i].connect(this.output)
         }
     }
+
+    addVoice(vcoType) {
+        this.freqScalars.push(new Tone.Multiply(1))
+        if (vcoType === 'noise') {
+            this.vco.push(new Tone.Noise("white").start())
+        }
+        else {
+            this.vco.push(new Tone.Oscillator({type:vcoType}).start())
+        }
+        this.gainStages.push(new Tone.Multiply(1))
+        this.frequency.connect(this.freqScalars[this.numInputs])
+        if (vcoType !== 'noise') {
+            this.freqScalars[this.numInputs].connect(this.vco[this.numInputs].frequency)
+        }
+        this.vco[this.numInputs].connect(this.gainStages[this.numInputs])
+        this.gainStages[this.numInputs].connect(this.output)
+        this.numInputs++
+        this.output.factor.value = 1/this.numInputs
+    }
+
+    removeVoice(index = 0) {
+        this.vco[index].stop()
+        this.frequency.disconnect(this.freqScalars[index])
+        if (this.vco[index].type !== 'noise') {
+            this.freqScalars[index].disconnect(this.vco[index].frequency)
+        }
+        this.vco[index].disconnect(this.gainStages[index])
+        this.gainStages[index].disconnect(this.output)
+        if (index < this.numInputs - 1) {
+            for (let i = index; i < this.numInputs ; i++) {
+                this.freqScalars[i] = this.freqScalars[i+1]
+                this.gainStages[i] = this.gainStages[i+1]
+                this.vco[i] = this.vco[i+1]
+            }
+        }
+        this.freqScalars.pop()
+        this.gainStages.pop()
+        this.vco.pop()
+
+        this.numInputs--
+        this.output.factor.value = this.numInputs === 0 ? 1 : 1/this.numInputs
+    }
+
     setPitchshift(index, shift) {
         if (index >= this.numInputs || index < 0) {
             console.log("Index out of range")

312cb766bfcaec234abfbeab3c9ab818bd8f32ee
ianhattwick <ian@ianhattwick.com>
2024-10-14 00:33:25 -0400

    Stereo delay!

--- src/synths/AnalogDelay.js
+++ src/synths/AnalogDelay.js
@@ -27,8 +27,12 @@ export class AnalogDelay {
     this.ws_input = new Tone.Multiply(0.125);
     this.waveShaper = new Tone.WaveShaper((x) => { return Math.tanh(x) });
     this.vcf = new Tone.Filter({ type: 'lowpass', frequency: 5000, Q: 0, slope: '-12' });
+    this.vcfR = new Tone.Filter({ type: 'lowpass', frequency: 5000, Q: 0, slope: '-12' });
     this.delay = new Tone.Delay(initialTime);
+    this.delayR = new Tone.Delay(initialTime);
     this.feedbackMult = new Tone.Multiply(initialFB);
+    this.feedbackMultR = new Tone.Multiply(initialFB);
+    this.merge = new Tone.Merge(2)
     this.wetSig = new Tone.Multiply(1);
     this.drySig = new Tone.Multiply(0);
     this.output = new Tone.Multiply(1);
@@ -39,17 +43,32 @@ export class AnalogDelay {
     this.highpass.connect(this.ws_input);
     this.ws_input.connect(this.waveShaper);
     this.waveShaper.connect(this.vcf);
+    this.waveShaper.connect(this.vcfR);
     this.vcf.connect(this.delay);
+    this.vcfR.connect(this.delayR);
     this.delay.connect(this.feedbackMult);
+    this.delayR.connect(this.feedbackMultR);
     this.feedbackMult.connect(this.vcf);
-    this.delay.connect(this.wetSig);
+    this.feedbackMultR.connect(this.vcfR);
+    this.delay.connect(this.merge,0,0);
+    this.delayR.connect(this.merge,0,1);
+    this.merge.connect(this.wetSig);
     this.wetSig.connect(this.output);
     this.drySig.connect(this.output);
+
+    this.lfo = new Tone.Oscillator(2).start()
+    this.lfoDepth = new Tone.Multiply()
+    this.lfo.connect(this.lfoDepth)
+    this.lfoDepth.connect(this.delay.delayTime)
+    this.lfoDepth.connect(this.delayR.delayTime)
   }
 
-  set time (value) {this.delay.delayTime.value = value}
-  set feedback (value) { this.feedbackMult.factor.value = value}
-  set damping (value) {this.vcf.frequency.value = value}
+  set time (value) {
+    this.delay.delayTime.value = value
+    this.delayR.delayTime.value = value*1.5
+  }
+  set feedback (value) { this.feedbackMult.factor.value = value; this.feedbackMultR.factor.value = value;}
+  set damping (value) {this.vcf.frequency.value = value; this.vcf.frequency.value = value*0.9;}
   set hpf (value) { this.highpass.frequency.value = value}
   set dry (value) { this.drySig.factor.value = value}
   set wet (value) { this.wetSig.factor.value = value}
